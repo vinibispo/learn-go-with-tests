7cbd3186227368c072358089a92a0130095989e4
vinibispo <vini.bispo015@gmail.com>
2024-06-26 13:06:55 -0300

    Refactor Repeat function

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,11 @@
 package iteration
 
+const repeatCount = 5
 func Repeat(character string) string {
   var repeated string
 
-  for i := 1; i < 5; i++ {
-    repeated = repeated + character
+  for i := 1; i < repeatCount; i++ {
+    repeated += character
   }
 
   return repeated
