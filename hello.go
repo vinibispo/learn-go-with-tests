db245a8f9ebabeddaec4269679d954b491457ff4
vinibispo <vini.bispo015@gmail.com>
2024-06-26 12:35:01 -0300

    Add support for different languages

--- hello.go
+++ hello.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-  return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+const portuguese = "Portuguese"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const portugueseHelloPrefix = "Olá, "
+func Hello(name string, language string) string {
+  if name == "" {
+    name = "World"
+  }
+
+  prefix := englishHelloPrefix
+
+  switch language {
+  case spanish:
+    prefix = spanishHelloPrefix
+  case french:
+    prefix = frenchHelloPrefix
+  case portuguese:
+    prefix = portugueseHelloPrefix
+  }
+
+  return prefix + name
 }
 
 func main() {
-  fmt.Println(Hello("world"))
+  fmt.Println(Hello("", ""))
 }
