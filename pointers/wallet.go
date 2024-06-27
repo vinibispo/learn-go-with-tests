b8f9d29b560ffac06528106882f0cc46ec409233
vinibispo <vini.bispo015@gmail.com>
2024-06-26 21:26:15 -0300

    Add way to throw error in Withdraw function

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 
 type Bitcoin int
@@ -19,4 +22,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Balance() Bitcoin {
   return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+  if amount > w.balance {
+    return errors.New("oh no")
+  }
+  w.balance -= amount
+  return nil
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,16 +3,41 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
+  assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+    t.Helper()
+    got := wallet.Balance()
 
-  wallet := Wallet{}
-
-  wallet.Deposit(Bitcoin(10))
-
-  got := wallet.Balance()
-
-  want := Bitcoin(10)
-
-  if got != want {
-    t.Errorf("got %s want %s", got, want)
+    if got != want {
+      t.Errorf("got %s want %s", got, want)
+    }
   }
+  t.Run("deposit", func(t *testing.T) {
+    wallet := Wallet{}
+
+    wallet.Deposit(Bitcoin(10))
+
+    assertBalance(t, wallet, Bitcoin(10))
+
+  })
+
+  t.Run("withdraw", func(t *testing.T) {
+    wallet := Wallet{balance: Bitcoin(20)}
+
+    wallet.Withdraw(Bitcoin(10))
+
+    assertBalance(t, wallet, Bitcoin(10))
+  })
+
+  t.Run("withdraw insufficient funds", func(t *testing.T) {
+    startingBalance := Bitcoin(20)
+    wallet := Wallet{startingBalance}
+
+    err := wallet.Withdraw(Bitcoin(100))
+
+    assertBalance(t, wallet, startingBalance)
+
+    if err == nil {
+      t.Error("wanted an error but didn't get one")
+    }
+  })
 }
