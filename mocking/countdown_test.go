a55d8ba665ab3795ffa947382107b62c926633a3
vinibispo <vini.bispo015@gmail.com>
2024-06-28 15:31:07 -0300

    Improve countdown_test by adding a test to tell if it's working from 3 to Go

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"slices"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-  t.Run("sleep before print", func(t *testing.T) {
+  t.Run("prints 3 to Go!", func(t *testing.T) {
+    buffer := &bytes.Buffer{}
+    Countdown(buffer, &SpyCountdownOperations{})
+    got := buffer.String()
+    want := `3
+2
+1
+Go!`
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
+  })
+  t.Run("sleep before every print", func(t *testing.T) {
     spySleepPrinter := &SpyCountdownOperations{}
     Countdown(spySleepPrinter, spySleepPrinter)
 
