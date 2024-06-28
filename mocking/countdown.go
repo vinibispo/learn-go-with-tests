4c10517efcaa21d9faa0d9c5c17760f04d6aa321
vinibispo <vini.bispo015@gmail.com>
2024-06-28 08:56:54 -0300

    Add mock to our countdown program

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,15 +4,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-  for i := 3; i > 0; i-- {
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+  Sleep()
+}
+
+type SpySleeper struct {
+  Calls int
+}
+
+func (s *SpySleeper) Sleep () {
+  s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+  time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+  for i := countdownStart; i > 0; i-- {
     fmt.Fprintln(out, i)
+    sleeper.Sleep()
   }
-  fmt.Fprint(out, "Go!")
+  fmt.Fprint(out, finalWord)
 }
 
 func main() {
-  Countdown(os.Stdout)
+  sleeper := &DefaultSleeper{}
+  Countdown(os.Stdout, sleeper)
 }
