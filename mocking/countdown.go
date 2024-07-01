1dc94518a3cab388613e7c067bca0701f5ec7e9f
vinibispo <vini.bispo015@gmail.com>
2024-07-01 08:09:04 -0300

    Remove DefaultSleeper and use ConfigurableSleeper in the production code

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,9 +14,6 @@ type Sleeper interface {
   Sleep()
 }
 
-type DefaultSleeper struct{}
-
-
 type ConfigurableSleeper struct {
   duration time.Duration
   sleep func(time.Duration)
@@ -24,10 +21,6 @@ type ConfigurableSleeper struct {
 
 func (c *ConfigurableSleeper) Sleep() {
   c.sleep(c.duration)
-}
-
-func (d *DefaultSleeper) Sleep() {
-  time.Sleep(1 * time.Second)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -39,6 +32,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-  sleeper := &DefaultSleeper{}
+  sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
   Countdown(os.Stdout, sleeper)
 }
