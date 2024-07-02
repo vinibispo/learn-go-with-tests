18b6d06e8c28f799590efd7bcf280cc352e3c782
vinibispo <vini.bispo015@gmail.com>
2024-07-02 07:59:28 -0300

    Finish select program configuring the timeout

--- select/racer_test.go
+++ select/racer_test.go
@@ -30,18 +30,15 @@ func TestRacer(t *testing.T) {
 		fastServer.Close()
 	})
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
-
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
-			t.Error("expected an error, but didn't get one")
+			t.Error("expected an error but didn't get one")
 		}
 	})
 }
