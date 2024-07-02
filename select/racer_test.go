7b50257852be4802f7ab7a8324d091203f317b31
vinibispo <vini.bispo015@gmail.com>
2024-07-02 07:30:04 -0300

    Make racer not be flaky test

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,14 +1,33 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-  slowUrl := "https://facebook.com"
-  fastUrl := "http://quii.dev"
+  slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    time.Sleep(20 * time.Millisecond)
+    w.WriteHeader(http.StatusOK)
+  }))
+
+  fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+  }))
+
+  slowUrl := slowServer.URL
+  fastUrl := fastServer.URL
+
   want := fastUrl
+
   got := Racer(slowUrl, fastUrl)
 
   if got != want {
     t.Errorf("got %q, want %q", got, want)
   }
+
+  slowServer.Close()
+  fastServer.Close()
 }
