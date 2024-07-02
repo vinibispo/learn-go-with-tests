6ca2d33cf1591633f34fcfc36cff77281d38d6b1
vinibispo <vini.bispo015@gmail.com>
2024-07-02 20:48:16 -0300

    Add Roman Numerals basic program

---
+++ roman-numerals/numeral_test.go
@@ -0,0 +1,17 @@
+package romannumerals
+
+import "testing"
+
+
+func TestRomanNumerals(t *testing.T) {
+  got := ConvertToRoman(1)
+  want := "I"
+
+  if got != want {
+    t.Errorf("got %q, want %q", got, want)
+  }
+}
+
+func ConvertToRoman(arabic int) string {
+  return "I"
+}
