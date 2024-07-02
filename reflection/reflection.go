9b14c76ea9362ee82792dded5fe5ff3dc78e8346
vinibispo <vini.bispo015@gmail.com>
2024-07-02 14:50:06 -0300

    Refactor fetch value using reflect.ValueOf

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,11 +3,7 @@ package main
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-	if val.Kind() == reflect.Pointer {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -18,4 +14,14 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
