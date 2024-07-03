9a2610f851651fb82382be0626cf4e0b92568c10
vinibispo <vini.bispo015@gmail.com>
2024-07-03 17:54:58 -0300

    Add PostRenderer in the blogrenderer package

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -16,14 +16,21 @@ var (
 	postTemplates embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
-	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+type PostRenderer struct {
+	templ *template.Template
+}
 
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
 		return err
 	}
 
