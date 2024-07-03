1fc397123c714dc3cdc2def39c4ce3cc9cec9908
vinibispo <vini.bispo015@gmail.com>
2024-07-02 21:57:03 -0300

    move cases outside TestConvertingToRomanNumerals

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
+var (
+	cases = []struct {
 		Arabic int
 		Roman  string
 	}{
@@ -41,7 +41,9 @@ func TestRomanNumerals(t *testing.T) {
 		{Arabic: 1006, Roman: "MVI"},
 		{Arabic: 798, Roman: "DCCXCVIII"},
 	}
+)
 
+func TestConvertingToRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
