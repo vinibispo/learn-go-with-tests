812618c548e4ddc3dec0f7262cae2b4cf29da39b
vinibispo <vini.bispo015@gmail.com>
2024-07-03 14:57:42 -0300

    Move newPost and Post struct to post.go

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,13 +1,8 @@
 package blogposts
 
 import (
-	"io"
 	"io/fs"
 )
-
-type Post struct {
-	Title string
-}
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
@@ -34,14 +29,4 @@ func getPost(fileSystem fs.FS, fileName string) (Post, error) {
 	}
 	defer postFile.Close()
 	return newPost(postFile)
-}
-
-func newPost(postFile io.Reader) (Post, error) {
-	postData, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
-	}
-
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
 }
