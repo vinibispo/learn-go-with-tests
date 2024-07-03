b1c95cdd1b418e6fc97c49dd82242848baf62fe4
vinibispo <vini.bispo015@gmail.com>
2024-07-03 20:52:33 -0300

    Add a bunch of abstractions in our bad_bank

--- arrays/bad_bank.go
+++ arrays/bad_bank.go
@@ -6,15 +6,30 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, t Transaction) float64 {
-		if t.From == name {
-			return currentBalance - t.Sum
-		}
-		if t.To == name {
-			return currentBalance + t.Sum
-		}
-		return currentBalance
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
 	}
-	return Reduce(transactions, adjustBalance, 0.0)
+
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
 }
