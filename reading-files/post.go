21a5f2166dec0cad47dbe078e856aa02093ddb6c
vinibispo <vini.bispo015@gmail.com>
2024-07-03 16:21:38 -0300

    Add readBody function

--- reading-files/post.go
+++ reading-files/post.go
@@ -29,10 +29,15 @@ func newPost(postBody io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	title := readMetaLine(titleSeparator)
-	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
 
+func readBody(scanner *bufio.Scanner) string {
 	scanner.Scan() // ignore a line
 
 	buf := bytes.Buffer{}
@@ -40,12 +45,5 @@ func newPost(postBody io.Reader) (Post, error) {
 		fmt.Fprintln(&buf, scanner.Text())
 	}
 
-	body := strings.TrimSuffix(buf.String(), "\n")
-
-	return Post{
-		Title:       title,
-		Description: description,
-		Tags:        tags,
-		Body:        body,
-	}, nil
+	return strings.TrimSuffix(buf.String(), "\n")
 }
