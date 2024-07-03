ea942313f1aed59dfe55e850e7a0c19071238621
vinibispo <vini.bispo015@gmail.com>
2024-07-03 19:47:07 -0300

    Add assert.go file to have the Assert helpers

---
+++ generics/assert.go
@@ -0,0 +1,17 @@
+package generics
+
+import "testing"
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %v", got)
+	}
+}
--- generics/generics_test.go
+++ generics/generics_test.go
@@ -13,17 +13,3 @@ func TestAssertFunctions(t *testing.T) {
 		AssertNotEqual(t, "hello", "Grace")
 	})
 }
-
-func AssertEqual[T comparable](t *testing.T, got, want T) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
-func AssertNotEqual[T comparable](t *testing.T, got, want T) {
-	t.Helper()
-	if got == want {
-		t.Errorf("didn't want %v", got)
-	}
-}
