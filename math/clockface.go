7327cb7d18bd69959affc8c6ea12e378ab1e117c
vinibispo <vini.bispo015@gmail.com>
2024-07-03 08:00:18 -0300

    Refactor SecondHand by getting rid of magic numbers

--- math/clockface.go
+++ math/clockface.go
@@ -10,11 +10,15 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCentreX = 150
+const clockCentreY = 150
+
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + 150, p.Y + 150}
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      //flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         //translate
 	return p
 }
 
