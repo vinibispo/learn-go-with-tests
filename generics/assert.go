2600142ce12202765638657f9c1bbb3ec28fb128
vinibispo <vini.bispo015@gmail.com>
2024-07-03 19:50:26 -0300

    Add AssertTrue and AssertFalse functions

--- generics/assert.go
+++ generics/assert.go
@@ -15,3 +15,17 @@ func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 		t.Errorf("didn't want %v", got)
 	}
 }
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
