044e900f8a71f3e580368a505cb26b2da21b8d98
vinibispo <vini.bispo015@gmail.com>
2024-07-02 22:24:04 -0300

    add assertion func in quick.Check

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -2,7 +2,9 @@ package romannumerals
 
 import (
 	"fmt"
+	"log"
 	"testing"
+	"testing/quick"
 )
 
 var (
@@ -62,5 +64,21 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic int) bool {
+		if arabic < 0 || arabic > 3999 {
+			log.Println(arabic)
+			return true
+		}
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
