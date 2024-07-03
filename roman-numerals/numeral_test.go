265caca11de794847e1a8b42b730477d26a592cf
vinibispo <vini.bispo015@gmail.com>
2024-07-02 21:04:29 -0300

    Make the ConvertToRoman works with the number 5

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -15,6 +15,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
 		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
 
 	for _, test := range cases {
@@ -44,6 +45,10 @@ func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
 	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
 		if i == 4 {
 			result.WriteString("IV")
 			break
