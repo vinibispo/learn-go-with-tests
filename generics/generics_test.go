82221ce47a6510a28886cb3928bb3c1bf4484fdc
vinibispo <vini.bispo015@gmail.com>
2024-07-03 19:10:54 -0300

    Add AssertEqual and AssertNotEqual for interface

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -7,16 +7,21 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
 	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
 }
 
-func AssertEqual(t *testing.T, got, want int) {
+func AssertEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want int) {
+func AssertNotEqual(t *testing.T, got, want interface{}) {
 	t.Helper()
 	if got == want {
 		t.Errorf("didn't want %d", got)
