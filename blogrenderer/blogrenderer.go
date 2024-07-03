85ecd733c6a4ca28faf4c805c06291a0f7a32eca
vinibispo <vini.bispo015@gmail.com>
2024-07-03 18:54:32 -0300

    Add way to parse markdown in Body of Post

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -5,16 +5,14 @@ import (
 	"html/template"
 	"io"
 	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 type Post struct {
 	Title, Description, Body string
 	Tags                     []string
-}
-
-type PostViewModel struct {
-	Title, SanitisedTitle, Description, Body string
-	Tags                                     []string
 }
 
 func (p Post) SanitisedTitle() string {
@@ -28,6 +26,7 @@ var (
 
 type PostRenderer struct {
 	templ *template.Template
+  mdParser *parser.Parser
 }
 
 func NewPostRenderer() (*PostRenderer, error) {
@@ -36,13 +35,27 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+  extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+  parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
 	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+  Post
+  HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+  vm := postViewModel{Post: p}
+  vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+  return vm
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/vinibispo/learn-go-with-tests
 
 go 1.22.1
 
-require github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958 // indirect
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20240626202925-2eda941fd024 // indirect
+)
