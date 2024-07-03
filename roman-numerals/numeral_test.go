26f4617d1beb876e4a71c616d694a41dd55398a4
vinibispo <vini.bispo015@gmail.com>
2024-07-02 21:01:57 -0300

    Refactor ConvertToRoman

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -40,13 +40,14 @@ func TestRomanNumerals(t *testing.T) {
 }
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
 
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
