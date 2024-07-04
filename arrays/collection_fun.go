50448ade4d8ba2050ce891b1cabc3a7b567f6efa
vinibispo <vini.bispo015@gmail.com>
2024-07-03 21:08:54 -0300

    Add Find abstraction

--- arrays/collection_fun.go
+++ arrays/collection_fun.go
@@ -8,3 +8,12 @@ func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	}
 	return result
 }
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
--- arrays/collection_fun_test.go
+++ arrays/collection_fun_test.go
@@ -19,3 +19,15 @@ func TestReduce(t *testing.T) {
 		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+}
