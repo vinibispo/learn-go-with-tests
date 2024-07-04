eae53ea53e548c752a7e64cc028623b34708be6a
vinibispo <vini.bispo015@gmail.com>
2024-07-04 07:14:15 -0300

    Add StatusNotFound in our ServeHTTP

--- http-server/server.go
+++ http-server/server.go
@@ -17,5 +17,7 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	w.WriteHeader(http.StatusNotFound)
+
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
