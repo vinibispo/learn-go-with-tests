01775a9b81568ea98939d9d02312829096b23ba6
vinibispo <vini.bispo015@gmail.com>
2024-07-04 10:26:36 -0300

    Add NewHttpServer to inject store and add ServeMux in our http-server outside of ServeHTTP

--- http-server/main.go
+++ http-server/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	store := NewInMemoryPlayerStore()
-	server := &PlayerServer{store}
+	server := NewPlayerServer(store)
 	handler := http.HandlerFunc(server.ServeHTTP)
 	log.Fatal(http.ListenAndServe(":8000", handler)) //setting 8000 because MacOS Control Center uses 5000
 }
