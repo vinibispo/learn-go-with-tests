c0566483f49d01faeab8cc5047f9947ba3ad5e1b
vinibispo <vini.bispo015@gmail.com>
2024-07-04 07:03:14 -0300

    Inject Store in main.go of http-server

--- http-server/main.go
+++ http-server/main.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	handler := http.HandlerFunc(server.ServeHTTP)
 	log.Fatal(http.ListenAndServe(":8000", handler)) //setting 8000 because MacOS Control Center uses 5000
 }
