6324fe27e988fca89e2ad3422daf6948cd287f95
vinibispo <vini.bispo015@gmail.com>
2024-07-03 23:14:49 -0300

    Add main.go file to call our http-server

---
+++ http-server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+  handler := http.HandlerFunc(PlayerServer)
+  log.Fatal(http.ListenAndServe(":8000", handler)) //setting 8000 because MacOS Control Center uses 5000
+}
