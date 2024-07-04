2ff904764f4521d4997ba17151fecac4b3cdea19
vinibispo <vini.bispo015@gmail.com>
2024-07-04 09:07:36 -0300

    Add RecordWin to the store in main.go, and in the server_test.go in http-server

--- http-server/server.go
+++ http-server/server.go
@@ -8,6 +8,7 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
@@ -36,5 +37,6 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 }
 
 func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	p.store.RecordWin("Bob")
 	w.WriteHeader(http.StatusAccepted)
 }
