33236782551139586da559641db5e47b80a05b29
vinibispo <vini.bispo015@gmail.com>
2024-07-05 15:23:10 -0300

    Get rid of InMemoryPlayerStore to use FileSystemPlayerStore

--- http-server/main.go
+++ http-server/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	store := NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
 	server := NewPlayerServer(store)
-	handler := http.HandlerFunc(server.ServeHTTP)
-	log.Fatal(http.ListenAndServe(":8000", handler)) //setting 8000 because MacOS Control Center uses 5000
+
+	//setting 8000 because MacOS Control Center uses 5000
+	if err := http.ListenAndServe(":8000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
