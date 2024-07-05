8d30e059ee59ca4697edefb8d2c9133c02a1c346
vinibispo <vini.bispo015@gmail.com>
2024-07-05 10:43:53 -0300

    Add test helper assertScoreEquals

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -33,11 +33,13 @@ func TestFileSystemStore(t *testing.T) {
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
-
 		want := 33
-
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertScoreEquals(t, got, want)
 	})
+}
+
+func assertScoreEquals(t testing.TB, got, want int) {
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
