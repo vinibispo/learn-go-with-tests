cb20b683c2819613b0492c336467520df00b1377
vinibispo <vini.bispo015@gmail.com>
2024-07-06 15:14:06 -0300

    Add os.Stdout in the cli entrypoint

--- http-server/cmd/cli/main.go
+++ http-server/cmd/cli/main.go
@@ -21,5 +21,5 @@ func main() {
 
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdoutAlerter)).PlayPoker()
+	poker.NewCLI(store, os.Stdin, os.Stdout, poker.BlindAlerterFunc(poker.StdoutAlerter)).PlayPoker()
 }
