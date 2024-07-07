9dda487004c3ce0fd08e5c3bd3426c900afe4420
vinibispo <vini.bispo015@gmail.com>
2024-07-06 23:29:05 -0300

    Create helper newGameRequest to make easy to retrieve the request

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -100,7 +100,7 @@ func TestGame(t *testing.T) {
 	t.Run("GET /game returns 200", func(t *testing.T) {
 		server := NewPlayerServer(&StubPlayerStore{})
 
-		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		request := newGameRequest()
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -113,6 +113,11 @@ func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want s
 	if response.Result().Header.Get("content-type") != want {
 		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
 	}
+}
+
+func newGameRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/game", nil)
+	return req
 }
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) []Player {
