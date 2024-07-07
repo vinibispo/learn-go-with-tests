8855653297deff3b0118dcda7d47cd788273b826
vinibispo <vini.bispo015@gmail.com>
2024-07-06 22:31:02 -0300

    Add GameSpy struct with Start and Finish functions

--- http-server/CLI_test.go
+++ http-server/CLI_test.go
@@ -8,6 +8,19 @@ import (
 	poker "github.com/vinibispo/learn-go-with-tests/http-server"
 )
 
+type GameSpy struct {
+	StartedWith  int
+	FinishedWith string
+}
+
+func (g *GameSpy) Start(numberOfPlayers int) {
+	g.StartedWith = numberOfPlayers
+}
+
+func (g *GameSpy) Finish(winner string) {
+	g.FinishedWith = winner
+}
+
 var dummyBlindAlerter = &poker.SpyBlindAlerter{}
 var dummyPlayerStore = &poker.StubPlayerStore{}
 var dummyStdIn = &bytes.Buffer{}
@@ -36,21 +49,23 @@ func TestCLI(t *testing.T) {
 		poker.AssertPlayerWin(t, playerStore, "Cleo")
 	})
 
-
-	t.Run("it prompts the user to enter the number of players", func(t *testing.T) {
+	t.Run("it prompts the user to enter the number of players and starts the game", func(t *testing.T) {
 		stdout := &bytes.Buffer{}
 		in := strings.NewReader("7\n")
-		blindAlerter := &poker.SpyBlindAlerter{}
+		game := &GameSpy{}
 
-		game := poker.NewTexasHoldem(blindAlerter, dummyPlayerStore)
 		cli := poker.NewCLI(in, stdout, game)
 		cli.PlayPoker()
 
-		got := stdout.String()
-		want := poker.PlayerPrompt
+		gotPrompt := stdout.String()
+		wantPrompt := poker.PlayerPrompt
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		if gotPrompt != wantPrompt {
+			t.Errorf("got %q, want %q", gotPrompt, wantPrompt)
+		}
+
+		if game.StartedWith != 7 {
+			t.Errorf("wanted start called with 7 but got %d", game.StartedWith)
 		}
 	})
 }
